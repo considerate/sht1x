849bf0bb1692121d9a29d0078e7cf21d8316e267
Viktor Kronvall <viktor.kronvall@gmail.com>
2014-04-22 13:40:58 +0200

    Adding callback to create()

--- test/sht1x_test.js
+++ test/sht1x_test.js
@@ -12,6 +12,7 @@ var tests = [{
 }];
 
 sht1x.create(function* (sensor) {
+	console.log('First test');
 	yield sensor.init({
 		dataPin: 15, 
 		clockPin: 18
@@ -23,18 +24,21 @@ sht1x.create(function* (sensor) {
 		console.log("%s: %d", test.label, test.convert(value));	
 	}
 	yield sensor.close();
+}, function() {
+	console.log('Second test');
+	sht1x.create(function* (sensor) {
+		yield sensor.init({
+			dataPin: 15, 
+			clockPin: 18
+		}); 
+		for(var i = 0; i < tests.length; i++) {
+			var test = tests[i];
+			var value = yield sensor.measure(test.type);
+			console.log("Data: %d", value);
+			console.log("%s: %d", test.label, test.convert(value));	
+		}
+		yield sensor.close();
+	});
 });
 
-sht1x.create(function* (sensor) {
-	yield sensor.init({
-		dataPin: 15, 
-		clockPin: 18
-	}); 
-	for(var i = 0; i < tests.length; i++) {
-		var test = tests[i];
-		var value = yield sensor.measure(test.type);
-		console.log("Data: %d", value);
-		console.log("%s: %d", test.label, test.convert(value));	
-	}
-	yield sensor.close();
-});
+
